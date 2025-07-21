99268f887fe18ae56012340d181c6418a58a4283
danalawati <dana.allawaty@gmail.com>
2025-07-21 15:00:49 +0400

    Add a Constructor

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,17 +1,20 @@
 public class BankAccount {
-    public class BankAccount {
 
-        String accountName;
-        String accountNumber;
-        double balance;
+    String accountName;
+    String accountNumber;
+    double balance;
 
-        public void showAccountInfo(){
+    public BankAccount(String accountName,String accountNumber,double balance){
+        this.accountName = accountName;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
 
-            System.out.println("Account Name: " + accountName);
-            System.out.println("Account Number: " + accountNumber);
-            System.out.println("Current Balance: " + balance);
-        }
     }
+    public void showAccountInfo(){
 
-
+        System.out.println("Account Name: " + accountName);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Current Balance: " + balance);
+    }
 }
+
