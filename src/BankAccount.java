229db9457a24c436bae00e2a5baffba9f1d0e79e
danalawati <dana.allawaty@gmail.com>
2025-07-21 14:58:04 +0400

    Create a Basic Class

---
+++ src/BankAccount.java
@@ -0,0 +1,17 @@
+public class BankAccount {
+    public class BankAccount {
+
+        String accountName;
+        String accountNumber;
+        double balance;
+
+        public void showAccountInfo(){
+
+            System.out.println("Account Name: " + accountName);
+            System.out.println("Account Number: " + accountNumber);
+            System.out.println("Current Balance: " + balance);
+        }
+    }
+
+
+}
